103f31366f1c25513b5ffd4517e6ad847a186232
xiongjun <bar@example.com>
2025-08-29 15:25:24 +0800

    feat: 定义TitleProps和ExamDesc

--- src/components/Leaderboard/Title.tsx
+++ src/components/Leaderboard/Title.tsx
@@ -1,13 +1,23 @@
 import { Brain } from "lucide-react";
 
-const Title: React.FC = () => {
+interface ExamDesc {
+  name: string,
+  desc: string
+}
+
+interface TitleProps {
+  examDesc: ExamDesc
+}
+
+
+const Title: React.FC<TitleProps> = ({ examDesc }) => {
   return (
     <div>
       <div className="flex gap-2 mb-2">
         <Brain className="w-5 h-5 mt-0.5 text-white/75" />
-        <span className="text-base font-medium text-white transition-colors text-pretty">Human's Last Exam</span>
+        <span className="text-base font-medium text-white transition-colors text-pretty">{examDesc.name}</span>
       </div>
-      <span className="text-sm transition-colors text-zinc-400">Challenging LLMs at the frontier of human knowledge</span>
+      <span className="text-sm transition-colors text-zinc-400">{examDesc.desc}</span>
     </div>
   );
 }
--- src/components/Leaderboard/index.tsx
+++ src/components/Leaderboard/index.tsx
@@ -2,6 +2,10 @@ import Title from "./Title";
 import ScoreItem from "./ScoreItem";
 
 const LeaderBoard: React.FC = () => {
+  const examDesc = {
+    name: "Human's Last Exam",
+    desc: "Challenging LLMs at the frontier of human knowledge"
+  }
   const entities = [
     {
       modelName: "gpt-5-2025-08-07",
@@ -17,7 +21,7 @@ const LeaderBoard: React.FC = () => {
   return (
     <a className="rounded-lg p-6 flex flex-col justify-between transition-all duration-300 
     bg-[#1A1A1A]/80 hover:bg-[#2A2A2A]/80 group relative overflow-hidden" href="">
-      <Title />
+      <Title examDesc={examDesc} />
       <div className="flex flex-col justify-between h-full">
         <div className="flex flex-col gap-3 mt-4 mb-4">
           {
