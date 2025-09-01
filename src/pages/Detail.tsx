529d95d21405edf4b4e71c5d19bcc9b7d9de5286
xj <foo@example.com>
2025-09-01 15:13:35 +0800

    cleanup: 删除注释掉的代码

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -7,18 +7,6 @@ const Detail: React.FC = () => {
   const { state } = useLocation();
   const entities = state as ModelScore[];
   return (
-    // <div className="flex bg-[#1A1A1A]/80">
-    //   <div className="flex-1 bg-amber-300">{id}</div>
-    //   <div className=" flex-2 flex flex-col gap-3 mt-4 mb-4">
-    //     {
-    //       entities.map((entity, index) => (
-    //         <div key={index} className="transition-opacity duration-300 border-white/10">
-    //           <ScoreItem entity={entity} />
-    //         </div>
-    //       ))
-    //     }
-    //   </div>
-    // </div>
     <div className="min-h-screen bg-black ">
       <div className="container py-8 mx-auto overflow-hidden lg:max-w-[1312px]">
         <h1 className="text-white text-4xl font-medium font-aeonik mb-8">{id}</h1>
