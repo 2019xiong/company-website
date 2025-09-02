7be9d70182365d346ee3bd1837b94433cc4412ee
xj <foo@example.com>
2025-09-02 11:17:41 +0800

    chore: 整理Title组件

--- src/components/Leaderboard/index.tsx
+++ src/components/Leaderboard/index.tsx
@@ -1,7 +1,23 @@
 import { Link } from "react-router-dom";
-import Title from "./Title";
+import { Brain } from "lucide-react";
 import ScoreItem from "./ScoreItem";
 import type { ModelScore, ExamDesc } from "../../types";
+
+interface TitleProps {
+  examDesc: ExamDesc
+}
+
+const Title: React.FC<TitleProps> = ({ examDesc }) => {
+  return (
+    <div>
+      <div className="flex gap-2 mb-2">
+        <Brain className="w-5 h-5 mt-0.5 text-white/75" />
+        <span className="text-base font-medium text-white transition-colors text-pretty">{examDesc.name}</span>
+      </div>
+      <span className="text-sm transition-colors text-zinc-400">{examDesc.desc}</span>
+    </div>
+  );
+}
 
 interface LeaderboardProps {
   examDesc: ExamDesc;
